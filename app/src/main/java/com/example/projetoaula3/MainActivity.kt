97817680bca9724a9cffe922027ba466b99d17a1
Sandro Nakamura <sandroarcadia@gmail.com>
2022-08-10 18:32:16 -0300

    Adicionando comentários ao MainActivity

--- app/src/main/java/com/example/projetoaula3/MainActivity.kt
+++ app/src/main/java/com/example/projetoaula3/MainActivity.kt
@@ -12,12 +12,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Atribuindo as propriedades do botão rodar a variável btnRodarNumero
         val btnRodarNumero = findViewById<Button>(R.id.btn_rodar)
 
+            // Aguarda o click no botão para rodar o bloco de código
             btnRodarNumero.setOnClickListener{
+                // Atribuindo as propriedades do textView nas variáveis
                 val texto = findViewById<TextView>(R.id.txt_nenhum_numero)
                 val resultado = findViewById<TextView>(R.id.txt_resultado)
+
+                // Gerando um número aleatório de 0 até 10
                 val numeroSorteado = Random().nextInt(11)
+
+                // Alterando o texto do textView do resultado com o valor sorteado
                 texto.setText(R.string.numero_sorteado)
                 resultado.text = numeroSorteado.toString()
 
